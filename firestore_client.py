5e72051142c2536d875a6883bcbacd1cdc63ee87
Antonio Brown <93072819+Antonio-Nyctophile@users.noreply.github.com>
2025-05-12 14:39:28 -0500

    Update firestore_client.py

--- firestore_client.py
+++ firestore_client.py
@@ -1,19 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import base64
+import os
 
-# Load Firebase Admin SDK
-cred = credentials.Certificate("serviceAccountKey.json")
+# Decode the base64 Firebase key into a temp file
+firebase_key_b64 = os.environ["FIREBASE_KEY_B64"]
+key_bytes = base64.b64decode(firebase_key_b64)
+
+with open("temp_firebase_key.json", "wb") as f:
+    f.write(key_bytes)
+
+cred = credentials.Certificate("temp_firebase_key.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 def fetch_movies():
-    movies_ref = db.collection('movies')
-    docs = movies_ref.stream()
-
-    movie_list = []
-    for doc in docs:
-        movie = doc.to_dict()
-        movie['id'] = doc.id
-        movie_list.append(movie)
-    return movie_list
+    movies_ref = db.collection("movies")
+    return [doc.to_dict() | {"id": doc.id} for doc in movies_ref.stream()]
